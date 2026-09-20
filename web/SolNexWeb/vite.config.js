3442273f792e30bc646c367f3b0234fb26df6605
Thisaru Rajapaksha <162295795+thisaru01@users.noreply.github.com>
2026-09-20 18:01:48 +0530

    feat: add shadcn for web app

--- web/SolNexWeb/vite.config.js
+++ web/SolNexWeb/vite.config.js
@@ -1,8 +1,14 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+    ]
+  }
 })
